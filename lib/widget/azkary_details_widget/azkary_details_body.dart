e75b5ce9521da424db5dbae02c58a4bc2a994727
mohamedamr77 <moamr947@gmail.com>
2024-04-30 06:35:32 +0300

    final sebha and azkar details and azkar screen and qibla

--- lib/widget/azkary_details_widget/azkary_details_body.dart
+++ lib/widget/azkary_details_widget/azkary_details_body.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import '../../core/image_core.dart';
 import '../../data_variable/azkar_model.dart';
 import '../../data_variable/zekr_model.dart';
 
 class CutomAzkaryDetails extends StatefulWidget {
-   CutomAzkaryDetails({super.key,required this.models,required this.title_appbar});
-  List<AzkarModel> models;
-  String title_appbar;
+  CutomAzkaryDetails({Key? key, required this.models, required this.title_appbar});
+  final List<AzkarModel> models;
+  final String title_appbar;
 
   @override
   State<CutomAzkaryDetails> createState() => _CutomAzkaryDetailsState();
 }
-
 class _CutomAzkaryDetailsState extends State<CutomAzkaryDetails> {
+  List<ZekrModel> zekrList = [];
+  int percentCounter = 0;
   @override
-  Widget build(BuildContext context) {
-    double changeValue=0;
-    List<ZekrModel>? zekrList=[];
+  void initState() {
+    super.initState();
     for (AzkarModel azkarModel in widget.models) {
       List<ZekrModel> zekrModels = azkarModel.zekr;
       zekrList.addAll(zekrModels);
-    }
 
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double percentage = percentCounter / zekrList.length;
     return Scaffold(
       body: Container(
         padding: EdgeInsets.only(
@@ -31,28 +37,27 @@ class _CutomAzkaryDetailsState extends State<CutomAzkaryDetails> {
           top: 16,
         ),
         decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(AppImage.background_Image),
-            )
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AppImage.background_Image),
+          ),
         ),
         child: CustomScrollView(
           slivers: [
             SliverAppBar(
-              title:Text(
+              title: Text(
                 "${widget.title_appbar}",
                 style: TextStyle(
-                    color: Color(0xff84835A),
-                    fontSize: 25,
-                    fontFamily: "Tajawal",
-                    fontWeight: FontWeight.bold
+                  color: Color(0xff84835A),
+                  fontSize: 25,
+                  fontFamily: "Tajawal",
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               centerTitle: true,
               leading: IconButton(
-                icon: Icon(  Icons.arrow_back_ios_new,
-                  color: Colors.black,),
-                onPressed: (){
+                icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+                onPressed: () {
                   Navigator.pop(context);
                 },
               ),
@@ -60,81 +65,66 @@ class _CutomAzkaryDetailsState extends State<CutomAzkaryDetails> {
               backgroundColor: Colors.transparent,
             ),
             SliverList.separated(
-              itemBuilder: (BuildContext context, int index) =>Container(child: Column(
+              itemBuilder: (BuildContext context, int index) => Container(
+                child: Column(
                   textDirection: TextDirection.rtl,
                   children: [
                     Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                  color: Color(0xff9E9660),
-                  borderRadius: BorderRadius.circular(20),
-                  ),
-                     child: Text('''${zekrList[index].title.trim()}''',
-                     textDirection: TextDirection.rtl,
-                  ),
+                      width: double.infinity,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xff9E9660),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        '${zekrList[index].title.trim()}',
+                        textDirection: TextDirection.rtl,
+                      ),
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(height: 10),
                     GFButtonBadge(
-                  color: Color(0xff9E9660),
-                  onPressed: () {
-                   setState(() {
-                     if(zekrList[index].number>0){
-                       zekrList[index].number--;
-                     }
-                   });
-                  },
-                  text: 'عدد مرات التكرار',
-                  icon: GFBadge(
-                  child: Text("${zekrList[index].number}",
-                  style: TextStyle(
-                  fontSize: 15,
-                  ),
-                  ),
-                  ),
-                  ),
-                    SizedBox(height: 8,),
-                    Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: GFProgressBar(
-                  percentage: changeValue ,
-                  backgroundColor : Colors.black26,
-                  progressBarColor: Color(0xff9E9660),
-                  leading: Icon(Icons.sentiment_dissatisfied_outlined,
-                  color:Color(0xff9E9660),
-                  ),
-                  clipLinearGradient: true,
-                  isDragable: true,
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  lineHeight: 25,
-                  circleWidth:100,
-                  animation:true,
-                  trailing:  Icon(Icons.sentiment_satisfied_alt,
-                  color: Color(0xff9E9660),
+                      color: Color(0xff9E9660),
+                      onPressed: () {
+                        setState(() {
+                          if (zekrList[index].number > 0) {
+                            zekrList[index].number--;
+                            if (zekrList[index].number == 0) {
+                              percentCounter++;
+                            }
+                          }
+                        });
+                      },
 
-                  ),
-                  child: Center(
-                  child: Text("${(changeValue * 100).toStringAsFixed(0)}%",
-                  textAlign: TextAlign.end,
-                  style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  ),
-                  ),
-                  ),
-                  ),
-                  ),
-                    SizedBox(height: 8,),
+
+                      text: 'عدد مرات التكرار',
+                      icon: GFBadge(
+                        child: Text(
+                          "${zekrList[index].number}",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    LinearPercentIndicator(
+                      lineHeight: 25,
+                      percent: percentage,
+                      progressColor: Colors.orange,
+                      backgroundColor: Colors.grey[400],
+                      barRadius: Radius.circular(20),
+                      center: Text("${(percentage * 100).toStringAsFixed(0)}%"),
+                    ),
+                    SizedBox(height: 8),
                     Divider(),
                   ],
-                  ),
-                  ),
-              separatorBuilder: (BuildContext context, int index) =>  SizedBox(height: 10,),
-              itemCount: widget.models.length,
+                ),
+              ),
+              separatorBuilder: (BuildContext context, int index) =>
+                  SizedBox(height: 10),
+              itemCount: zekrList.length,
             )
           ],
         ),
-      )
-    );}
+      ),
+    );
+  }
 }
--- lib/widget/sebha_widget/sebha_body.dart
+++ lib/widget/sebha_widget/sebha_body.dart
@@ -1,6 +1,6 @@
-import 'package:circular/circular.dart';
 import 'package:flutter/material.dart';
-
+import 'package:getwidget/getwidget.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import '../../core/image_core.dart';
 
 class SebhaBody extends StatefulWidget {
@@ -11,10 +11,18 @@ class SebhaBody extends StatefulWidget {
 }
 
 class _SebhaBodyState extends State<SebhaBody> {
-   double counter=0 ; // Initial value
-
+  double counter = 0; // Initial value
+  double maxCounter = 33;// Maximum value for counter
+  int index=0;
   @override
   Widget build(BuildContext context) {
+    double percentage = counter / maxCounter; // Calculate the percentage
+    List<String> list=[
+      "سبحان الله",
+      "الحمد الله",
+      "الله اكبر",
+      "لا اله الا الله"
+    ];
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -46,53 +54,132 @@ class _SebhaBodyState extends State<SebhaBody> {
               backgroundColor: Colors.transparent,
             ),
             SliverToBoxAdapter(
-              child: GestureDetector(
-                onTap: () {
-                  {
-                    if (counter < 33) {
-                    setState(() {
-                      counter++;
-                      print(counter);
-                    });
-                    } else {
-                      // Reset counter if it reaches the maximum value
-                     setState(() {
-                       counter = 0;
-                     });
-                    }
-                  }
-                },
-                child: CircularViewer(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(200),
-                    boxShadow: const [
-                      BoxShadow(
-                        offset: Offset(-10, -10),
-                        color: Colors.white,
-                        blurRadius: 20,
-                        spreadRadius: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height*0.18,),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (counter < maxCounter) {
+                          counter++;
+                          print(counter);
+                        } else {
+                          // Reset counter if it reaches the maximum value
+                          counter = 0;
+                          index++;
+                          if (index >= list.length) {
+                            index = 0; // Reset index if it exceeds the list length
+                          }
+                        }
+                      });
+                    },
+                    child: Center(
+                      child: CircularPercentIndicator(
+                        radius: 100,
+                        lineWidth: 25,
+                        percent: percentage,
+                        progressColor: Colors.amber,
+                        backgroundColor: Colors.black26,
+                        circularStrokeCap: CircularStrokeCap.round,
+                        center: Text(
+                          "${counter.toInt()}",
+                          style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 60,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
                       ),
-                      BoxShadow(
-                        offset: Offset(10, 10),
-                        color: Color.fromARGB(255, 158, 158, 158),
-                        blurRadius: 20,
-                        spreadRadius: 1,
-                      ),
-                    ],
+                    ),
                   ),
-                  value: counter,
-                  maxValue: 33,
-                  radius: 100,
-                  textStyle: TextStyle(fontSize: 30),
-                  color: Color(0xffEEEEEE),
-                  sliderColor: Color(0xff62CBDA),
-                  unSelectedColor: Color(0xffD7DEE7),
-                ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: (){
+                      setState(() {
+                      });
+                    }, child:
+                  Text(list[index],
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: "Tajawal"
+                    ),
+                  ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff3C312F),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(60),
+                            bottomLeft: Radius.circular(4),
+                            topLeft:Radius.circular(60) ,
+                            topRight: Radius.circular(4),
+                          ),
+                        ),
+                        minimumSize: Size(260, 44)
+                    ),
+                  ),
+                ],
               ),
-            ),
+            )
           ],
-        ),
+        )
       ),
     );
   }
 }
+
+
+/*
+ onTap: () {setState(() {
+                    if (counter < 33) {
+                      counter++;
+                      print(counter);
+                    } else {
+                      // Reset counter if it reaches the maximum value
+                      counter = 0;
+                    }
+                  });
+ */
+
+
+/*
+Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(20),
+                  width: 200, // Adjust width as needed
+                  height: 200, // Adjust height as needed
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xff62CBDA), // Change color as needed
+                  ),
+                  child: Text(
+                    counter.toString(),
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ),
+ */
+
+
+
+/*
+ SliverAppBar(
+              title: Text(
+                "سبحه",
+                style: TextStyle(
+                  color: Color(0xff84835A),
+                  fontSize: 25,
+                  fontFamily: "Tajawal",
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              centerTitle: true,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+            ),
+ */
